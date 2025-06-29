6f34858a583b2ac4d9e89c1e2f4a256382a800d6
Matheus Furlan <matheus.furlan7@hotmail.com>
2025-06-29 15:38:41 -0300

    feat(compress): implement response compression

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -4,6 +4,7 @@ import logger from '@plugins/logger';
 import { metricsPlugin } from '@plugins/metrics';
 import { rateLimitPlugin } from './plugins/rate-limit';
 import { corsPlugin } from './plugins/cors';
+import { compressPlugin } from './plugins/compress';
 
 import healthRoutes from '@modules/health/router/health.router';
 
@@ -12,6 +13,7 @@ const app = Fastify({ logger });
 metricsPlugin(app);
 rateLimitPlugin(app);
 corsPlugin(app);
+compressPlugin(app);
 
 app.register(healthRoutes, { prefix: '/health' });
 
