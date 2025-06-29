089e84f4952681dcdb6f022fe11734a42cfbbb6f
Matheus Furlan <matheus.furlan7@hotmail.com>
2025-06-29 15:16:27 -0300

    feat(rateLimit): configure rate limiting for endpoints

--- src/infra/http/app.ts
+++ src/infra/http/app.ts
@@ -1,10 +1,15 @@
 import Fastify from 'fastify';
 
 import logger from '@plugins/logger';
+import { metricsPlugin } from '@plugins/metrics';
+import { rateLimitPlugin } from './plugins/rate-limit';
 
 import healthRoutes from '@modules/health/router/health.router';
 
 const app = Fastify({ logger });
+
+metricsPlugin(app);
+rateLimitPlugin(app);
 
 app.register(healthRoutes, { prefix: '/health' });
 
